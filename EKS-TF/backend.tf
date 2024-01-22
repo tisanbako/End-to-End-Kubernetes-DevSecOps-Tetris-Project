806eb74df1fe9e255ea365c4b2a340e19d50a046
tisanbako <bakotisan@gmail.com>
2024-01-21 21:36:55 -0500

    updating files

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "tisan-backend-bucket"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "tisan-lock"
     encrypt        = true
   }
   required_version = ">=0.13.0"
